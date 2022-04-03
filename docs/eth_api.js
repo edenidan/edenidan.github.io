2bc5896ede2e346dc28e5dc6ce52cf46427cdafc
Idan Eden <37079533+edenidan@users.noreply.github.com>
2022-04-03 21:46:47 +0300

    Update eth_api.js

--- docs/eth_api.js
+++ docs/eth_api.js
@@ -1,6 +1,6 @@
 function usd_balance(address){
 
-    return 23;
+    return 23 + Math.random()*20;
     
     $.get(`https://thingproxy.freeboard.io/fetch/https://www.blockchain.com/eth/address/${address}`, (response) => {
         console.log(response);
